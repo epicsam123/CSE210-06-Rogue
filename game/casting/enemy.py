f72018ff754559088b463db84132d5bca7c03dff
James Chan <jhchan99@gmail.com>
2022-03-23 19:51:43 -0600

    changes to enemy movement in enemy

--- game/casting/enemy.py
+++ game/casting/enemy.py
@@ -1,6 +1,62 @@
+from decimal import DivisionByZero
+import re
+from turtle import position
 from game.casting.actor import Actor
+from game.casting.player import Player
+from game.shared.point import Point
 
 
 class Enemy(Actor):
     def __init__(self):
         super().__init__()
+
+    
+
+    
+    def get_position(self):
+        return super().get_position()
+
+    def get_velocity(self):
+        return super().get_velocity()
+    
+    def move_next(self):
+        return super().move_next()
+
+    def _make_vector(self, speed):
+        # get player position and own position and find slope
+        hero_x = player.get_position().get_x()
+        hero_y = player.get_position().get_y()
+        enemy_x = enemy.get_position().get_x()
+        enemy_y = enemy.get_position().get_y()
+
+        try:
+            vector_angle = (hero_y - enemy_y) / (hero_x - enemy_x)
+        except DivisionByZero:
+            pass
+        
+        new_enemy_x = vector_angle * enemy_x
+
+        vector_point = Point(new_enemy_x, new_enemy_y)
+
+        def direction_x(x):
+            if hero_x > enemy_x:
+                return x
+            elif hero_x < enemy_x:
+                return -x
+            else:
+                return 0
+           
+        
+
+        return vector_point
+        
+
+
+
+
+
+
+
+
+
+    
