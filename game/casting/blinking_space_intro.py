0ef7e08ceb8eee2347aa0cb9fcd532ccde990404
Samuel Casellas <samcomon@gmail.com>
2022-03-31 08:13:17 -0600

    My attempts to make a startup screen so far

---
+++ game/casting/blinking_space_intro.py
@@ -0,0 +1,26 @@
+from matplotlib.pyplot import show
+from game.casting.actor import Actor
+from game.shared.point import Point
+import constants
+
+class BlinkingSpaceIntro(Actor):
+    def __init__(self):
+        super().__init__()
+        self._showing = True
+        self._text = "Press Space to start"
+        self.set_position(Point(constants.MAX_X/2, constants.MAX_Y*.75))
+    
+    def change_blink_state(self):
+        def show_text():
+            if self._showing:
+                self._text = "Press Space to start"
+            else:
+                self._text = ""
+
+        self._showing = not self._showing
+        show_text()
+        
+
+    
+
+    
